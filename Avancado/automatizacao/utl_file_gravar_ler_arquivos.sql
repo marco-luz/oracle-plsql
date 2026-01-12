eec4e0ac286f7c4644a07f2863b8469a64cff1c4
marco-luz <marcodaluz@gmail.com>
2026-01-12 15:28:45 -0300

    study(pl/sql-avancado): UTL_FILE Gravar e ler arquivos
    - Criar o objeto diretorio no Windows
    - Gravando um arquivo Texto
    - Lendo um arquivo Texto

---
+++ Avancado/automatizacao/utl_file_gravar_ler_arquivos.sql
@@ -0,0 +1,75 @@
+-- Package UTL_FILE
+
+
+-- Criar o diretorio 'C:\ARQUIVOS_TESTE' no Windows
+-- Conectar como SYS
+
+CREATE OR REPLACE DIRECTORY ARQUIVOS_TESTE AS 'C:\ARQUIVOS_TESTE';
+
+GRANT READ, WRITE ON DIRECTORY ARQUIVOS_TESTE TO hr;
+
+
+
+-- Gravando um arquivo Texto
+
+SET SERVEROUTPUT ON
+SET VERIFY OFF
+DECLARE
+    vfile  UTL_FILE.FILE_TYPE;
+    CURSOR employees_c IS
+    SELECT employee_id, first_name, last_name, job_id, salary
+    FROM   employees;
+BEGIN
+    vfile := UTL_FILE.FOPEN('ARQUIVOS_TESTE', 'employees.txt','w',32767);
+    FOR employees_r IN employees_c LOOP
+        UTL_FILE.PUT_LINE(vfile, employees_r.employee_id || ';' || 
+                                 employees_r.first_name || ';' || 
+                                 employees_r.last_name || ';' ||
+                                 employees_r.job_id || ';' || 
+                                 employees_r.salary);
+    END LOOP;
+    UTL_FILE.FCLOSE(vfile);
+    DBMS_OUTPUT.PUT_LINE('Arquivo Texto employees.txt gerado com sucesso');
+EXCEPTION
+    WHEN UTL_FILE.INVALID_PATH THEN
+         UTL_FILE.FCLOSE(vfile);
+         DBMS_OUTPUT.PUT_LINE('Diretorio Invalido');
+    WHEN UTL_FILE.INVALID_OPERATION THEN
+         UTL_FILE.FCLOSE(vfile);
+         DBMS_OUTPUT.PUT_LINE('Operacao invalida no arquivo');
+    WHEN UTL_FILE.WRITE_ERROR THEN
+         UTL_FILE.FCLOSE(vfile);
+         DBMS_OUTPUT.PUT_LINE('Erro de gravacao no arquivo');     
+    WHEN UTL_FILE.INVALID_MODE THEN
+         UTL_FILE.FCLOSE(vfile);
+         DBMS_OUTPUT.PUT_LINE('Modo de acesso invalido');
+    WHEN OTHERS THEN
+         UTL_FILE.FCLOSE(vfile);
+         DBMS_OUTPUT.PUT_LINE('Erro Oracle:' || SQLCODE || SQLERRM);
+END;
+
+-- Lendo um arquivo Texto
+
+SET SERVEROUTPUT ON
+SET VERIFY OFF
+DECLARE
+    vfile  UTL_FILE.FILE_TYPE;
+    vregistro  VARCHAR2(400);
+BEGIN
+    vfile := UTL_FILE.FOPEN('ARQUIVOS_TESTE', 'employees.txt','r',32767);
+    LOOP
+        UTL_FILE.GET_LINE(vfile, vregistro);
+        DBMS_OUTPUT.PUT_LINE(vregistro);
+    END LOOP;
+
+EXCEPTION
+    WHEN NO_DATA_FOUND THEN
+         UTL_FILE.FCLOSE(vfile);
+         DBMS_OUTPUT.PUT_LINE('Arquivo Texto employees.txt lido com sucesso');
+    WHEN UTL_FILE.INVALID_PATH THEN
+         UTL_FILE.FCLOSE(vfile);
+         DBMS_OUTPUT.PUT_LINE('Diretorio Invalido');
+    WHEN OTHERS THEN
+         UTL_FILE.FCLOSE(vfile);
+         DBMS_OUTPUT.PUT_LINE('Erro Oracle:' || SQLCODE || SQLERRM);
+END;
